f03d26d782fd6934a6dc2585ad15a6e52e24bef2
kyawkyawlin <kyawkyawlin19954@gmail.com>
2024-05-21 01:57:16 +0700

    ep4 api v1

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\V1\TicketController;
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,8 +24,7 @@ use Illuminate\Support\Facades\Route;
 // tickets
 // users
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::apiResource('tickets', TicketController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
