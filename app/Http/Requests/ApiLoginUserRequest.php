51abe88984a7b2d74081ad5573cad69f299a4b67
kyawkyawlin <kkyawlin.connetis@gmail.com>
2024-06-09 23:07:05 +0700

    add login request file

--- app/Http/Requests/ApiLoginUserRequest.php
+++ app/Http/Requests/ApiLoginUserRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\Api\V1;
+namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateTicketRequest extends FormRequest
+class ApiLoginUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
