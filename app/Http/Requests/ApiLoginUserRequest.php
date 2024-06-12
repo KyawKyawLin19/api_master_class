4e62384554e687c791e4190b2485ec3ddcad3439
kyawkyawlin <kyawkyawlin19954@gmail.com>
2024-06-13 02:16:21 +0700

    ep5 auth token

--- app/Http/Requests/ApiLoginUserRequest.php
+++ app/Http/Requests/ApiLoginUserRequest.php
@@ -13,7 +13,7 @@ class ApiLoginUserRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class ApiLoginUserRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string', 'min:8'],
         ];
     }
 }
