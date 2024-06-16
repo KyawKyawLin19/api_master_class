9333c94d39efa82ee04c59b35629a6480021f6f9
kyawkyawlin <kyawkyawlin19954@gmail.com>
2024-06-16 23:54:01 +0700

    change wrap name

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
+use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
 
 class TicketController extends Controller
@@ -16,9 +17,10 @@ class TicketController extends Controller
      */
     public function index()
     {
-        return response()->json([
-            'data' => Ticket::all()
-        ]);
+        return TicketResource::collection(Ticket::all());
+        // return response()->json([
+        //     'data' => Ticket::all()
+        // ]);
     }
 
     /**
